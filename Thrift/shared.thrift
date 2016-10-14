eae42a53404640ef832f96041b3d02aaed860a4b
= <=>
2016-10-14 17:06:43 +0800

    First Thrift draft, not workable yet.

---
+++ Thrift/shared.thrift
@@ -0,0 +1,15 @@
+/**
+ * This Thrift file can be included by other Thrift files that want to share
+ * these definitions.
+ */
+
+namespace cpp shared
+
+struct SharedStruct {
+  1: i32 key
+  2: string value
+}
+
+service SharedService {
+  SharedStruct getStruct(1: i32 key)
+}
